76520275d61799cd14bb2eb1c3c6dd3db2dffd44
marwan ebrahim <marwanebrahim2006@gmail.com>
2026-04-22 21:25:55 +0200

    feat: Integrate Firebase for core functionalities and add necessary configurations

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fluid_boutique/core/helpers/hive_helper.dart';
 import 'package:fluid_boutique/features/app/data/datasource/app_local_data_source.dart';
 import 'package:fluid_boutique/features/app/data/datasource/app_remote_data_source.dart';
 import 'package:fluid_boutique/features/app/data/repository/app_repository.dart';
 import 'package:fluid_boutique/features/app/presentation/bloc/app_bloc.dart';
+import 'package:fluid_boutique/firebase_options.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -10,6 +12,7 @@ final sl = GetIt.instance;
 Future<void> initDependencies() async {
   // app features
   await HiveHelper.init();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   sl.registerLazySingleton<AppLocalDataSource>(
     () => AppLocalDataSourceImplWithHive(),
   );
